8ed654fa195bd762612615854b4c2b16b0f0ae64
Reina Nishi <s1f101901195@iniad.org>
2021-08-13 23:27:47 +0900

    login

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('app01/', include('authtest.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path(r'', include('TotalWeb.urls')),
 ]
